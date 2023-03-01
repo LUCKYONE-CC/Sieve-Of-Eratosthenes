7a2a1dcca14378f98d1466da11702c5b4dfa1ebb
Tom Schmitz <tom.schmitz16@gmail.com>
2023-03-01 20:57:58 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Sieb_des_Eratosthenes
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach(var item in SieveOfEratosthenes(1000000))
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public static List<int> SieveOfEratosthenes(int n)
+        {
+            bool[] prime = new bool[n + 1];
+
+            for (int i = 0; i < n + 1; i++)
+            {
+                prime[i] = true;
+            }
+
+            for (int p = 2; p * p <= n; p++)
+            {
+                if (prime[p] == true)
+                {
+                    for (int i = p * p; i <= n; i += p)
+                    {
+                        prime[i] = false;
+                    }
+                }
+            }
+
+            List<int> primes = new List<int>();
+
+            for (int p = 2; p <= n; p++)
+            {
+                if (prime[p] == true)
+                {
+                    primes.Add(p);
+                }
+            }
+
+            return primes;
+        }
+
+    }
+}
